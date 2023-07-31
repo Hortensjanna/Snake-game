77e5de033d57b13066dc8238a2c5f8f4fe0a5181
Ania Płecka <aniaplecka@gmail.com>
2023-07-31 11:57:21 +0200

    I added high score tracking

--- scoreboard.py
+++ scoreboard.py
@@ -9,20 +9,26 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.hideturtle()
         self.color('gray20')
-        self.goto(-245, 270)
+        self.goto(-140, 270)
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f'SCORE: {self.score}', False, align=ALIGMENT, font=FONT)
+        self.clear()
+        self.write(f'SCORE: {self.score}        HIGH SCORE: {self.high_score}', False, align=ALIGMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def track_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", False, align=ALIGMENT, font=FONT)
+
